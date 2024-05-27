0a55de454c20d8185af2554940c22610914d7c0f
Jesse Pazdera <jessepazdera@gmail.com>
2024-05-27 19:21:31 -0400

    Switched to ANOVA for analyzing C

--- E2/analysis/MixedEffectE2.R
+++ E2/analysis/MixedEffectE2.R
@@ -1,31 +1,15 @@
-library(sjstats)
-library(dplyr)
 library(tidyverse)
+library(sjstats)  # For anova_stats
+library(sjPlot)  # For plot_model
 library(lme4)  # For linear mixed effects modeling
 library(lmerTest)  # For F testing of LMER models
-library(car)
 
 #setwd("/Users/oliverinaldi/Documents/GitHub/IllusoryPitch/E2/data")
-setwd("/Users/jpazd/Documents/git/IllusoryPitch/E2/data")
+setwd("~/git/IllusoryPitch/E2/data")
 options(contrasts=c("contr.sum", "contr.poly"))
 
 ###
-# Figure 4 Analysis
-###
-
-data <- read.csv("fits.csv")
-data <- data %>% filter(!subject %in% c(21) & version >= 1.1 & shift_size == 1)
-cor.test(data$jnd, data$C_slope)
-# t = -0.75802, df = 25, p-value = 0.4555
-# alternative hypothesis: true correlation is not equal to 0
-# 95 percent confidence interval:
-# -0.5013479  0.2440226
-# sample estimates:
-# cor
-# -0.149891
-
-###
-# Figure 5 & 6 Analysis
+# Figure 4 & 5 Analysis
 ###
 
 dat <- read.csv("scores.csv")
@@ -46,9 +30,29 @@ anova_stats(model)
 # subject:offset:shift_size | offset:shift_size |  2 |  0.077 |  0.039 |     0.180 |   0.836 | 0.001 |         0.007 |  -0.002 |          -0.010 |    -0.002 |    0.083 | 0.078
 # subject:offset:shift_size |         Residuals | 52 | 11.177 |  0.215 |           |         |       |               |         |                 |           |          |
 
-data <- read.csv("fits.csv")
-data <- data %>% filter(!subject %in% c(21) & version >= 1.1)
-t.test(data[data$shift_size==1, 'C_slope'], data[data$shift_size==0.5, 'C_slope'], paired=TRUE)
+model <- aov(C ~ 1 + offset * shift_size + Error(subject / (offset * shift_size)), data=dat2)
+anova_stats(model)
+# etasq | partial.etasq | omegasq | partial.omegasq | epsilonsq | cohens.f |                     group |              term |  sumsq | df | meansq | statistic | p.value | power
+# -----------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+# 0.465 |         0.812 |   0.411 |           0.551 |     0.412 |    2.079 |                   subject |         Residuals | 13.108 | 26 |  0.504 |           |         | 1.000
+# 0.028 |         0.208 |   0.024 |           0.067 |     0.024 |    0.513 |            subject:offset |            offset |  0.797 |  2 |  0.398 |     4.672 |   0.014 | 0.921
+# 0.157 |         0.594 |   0.050 |           0.129 |     0.050 |    1.209 |            subject:offset |         Residuals |  4.435 | 52 |  0.085 |           |         | 1.000
+# 0.062 |         0.366 |   0.060 |           0.152 |     0.060 |    0.760 |        subject:shift_size |        shift_size |  1.751 |  1 |  1.751 |     9.882 |   0.004 | 1.000
+# 0.164 |         0.603 |   0.109 |           0.246 |     0.110 |    1.232 |        subject:shift_size |         Residuals |  4.607 | 26 |  0.177 |           |         | 1.000
+# 0.015 |         0.125 |   0.011 |           0.032 |     0.011 |    0.378 | subject:offset:shift_size | offset:shift_size |  0.433 |  2 |  0.217 |     3.715 |   0.031 | 0.682
+#       |               |         |                 |           |          | subject:offset:shift_size |         Residuals |  3.033 | 52 |  0.058 |           |         |
+
+# Pairwise t-tests for main effect of timing offset on C
+pairwise.t.test(dat2$C, dat2$offset, p.adj="holm", paired=T, alternative="two.sided")
+#    -15    0
+# 0  0.9516 -
+# 15 0.0194 0.0053
+# P value adjustment method: holm
+
+# Analysis of the offset x shift size interaction
+fits <- read.csv("fits.csv")
+fits <- fits %>% filter(!subject %in% c(21) & version >= 1.1)
+t.test(fits[fits$shift_size==1, 'C_slope'], fits[fits$shift_size==0.5, 'C_slope'], paired=TRUE)
 # t = 0.17829, df = 26, p-value = 0.8599
 # alternative hypothesis: true mean difference is not equal to 0
 # 95 percent confidence interval:
@@ -57,53 +61,17 @@ t.test(data[data$shift_size==1, 'C_slope'], data[data$shift_size==0.5, 'C_slope'
 # mean difference
 # 0.0005720765
 
-t.test(data$C_slope, mu=0)
-# t = 2.6844, df = 53, p-value = 0.009676
-# alternative hypothesis: true mean is not equal to 0
+###
+# Figure 6 Analysis
+###
+
+data <- read.csv("fits.csv")
+data <- data %>% filter(!subject %in% c(21) & version >= 1.1 & shift_size == 'pooled')
+cor.test(data$jnd, data$C_slope)
+# t = 0.13981, df = 25, p-value = 0.8899
+# alternative hypothesis: true correlation is not equal to 0
 # 95 percent confidence interval:
-# 0.001267931 0.008762749
+# -0.3558430  0.4036769
 # sample estimates:
-# mean of x
-# 0.00501534
-
-dat2$offset <- as.factor(dat2$offset)
-modelC <- lmer(C ~ 1 + poly(offset, 1) * shift_size + (1 + poly(offset, 1) + shift_size | subject),
-              data=dat2, REML=FALSE)
-anova(modelC)
-library(sjPlot)
-
-# Plot the random slopes
-plot_model(modelC, type = "est")
-
-poly(grp.means <- with(dat2,
-                   tapply(C, list(interval, difficulty), mean)))
-with(dat2,
-     interaction.plot(x.factor=interval,
-                      trace.factor=difficulty,
-                      response=C))
-#C
-# Type III Analysis of Variance Table with Satterthwaite's method
-#                              Sum Sq Mean Sq NumDF  DenDF F value    Pr(>F)    
-# poly(interval, 1)            1.0393  1.0393     1 145.02 12.3793 0.0005801 ***
-# difficulty                   1.2791  1.2791     1 160.84 15.2345 0.0001394 ***
-# poly(interval, 1):difficulty 0.0015  0.0015     1 145.02  0.0179 0.8937508 
-
-dat2$interval <- as.factor(dat2$interval)
-modeldprime <- lmer(dprime ~ 1 + interval * difficulty + (1 | subject),
-              data=dat2, REML=FALSE)
-anova(modeldprime)
-
-poly(grp.means <- with(dat2,
-                       tapply(dprime, list(interval, difficulty), mean)))
-with(dat2,
-     interaction.plot(x.factor=interval,
-                      trace.factor=difficulty,
-                      response=dprime))
-# Dprime
-# Type III Analysis of Variance Table with Satterthwaite's method
-#                      Sum Sq Mean Sq NumDF  DenDF F value Pr(>F)    
-# interval             0.7751  0.3875     2 145.37  1.4204 0.2449    
-# difficulty          27.2652 27.2652     1 152.50 99.9328 <2e-16 ***
-# interval:difficulty  0.2794  0.1397     2 145.37  0.5120 0.6004    
-# ---
-# Signif. codes:  0 ‘***’ 0.001 ‘**’ 0.01 ‘*’ 0.05 ‘.’ 0.1 ‘ ’ 1
+# cor
+# 0.02795063
